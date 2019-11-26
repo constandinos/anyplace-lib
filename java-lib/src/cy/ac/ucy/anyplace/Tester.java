aee12dd35de73a9b32a5943d1f2c6ab879dc255a
mchara01 <mchara01@cs.ucy.ac.cy>
2019-11-27 00:47:30 +0200

    WiFi scanning for linux finalized.

--- java-lib/src/cy/ac/ucy/anyplace/Tester.java
+++ java-lib/src/cy/ac/ucy/anyplace/Tester.java
@@ -2,6 +2,7 @@ package cy.ac.ucy.anyplace;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Tester {
 
@@ -22,28 +23,6 @@ public class Tester {
 		String access_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhjNThlMTM4NjE0YmQ1ODc0MjE3MmJkNTA4MGQxOTdkMmIyZGQyZjMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNTg3NTAwNzIzOTcxLXNpOHM0cXFhdDl2NWVmZ2VtbmViaWhwaTNxZTlvbmxwLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNTg3NTAwNzIzOTcxLXNpOHM0cXFhdDl2NWVmZ2VtbmViaWhwaTNxZTlvbmxwLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA0NDQxMzA0OTI3MzE2MzM5NDM2IiwiZW1haWwiOiJhY2hpbC5jaHJpc3Rvc0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6InpSVUJ4cVBjT29xejB0cVpkNEg1WnciLCJuYW1lIjoiY2hyaXN0b3MgYWNoaWxsZW9zIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8tVTVqVzlpRk9kRVEvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUNIaTNyYzZfTEEzLWV2dGFJbXVTdDU0cFJRdmd1T1BOQS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiY2hyaXN0b3MiLCJmYW1pbHlfbmFtZSI6ImFjaGlsbGVvcyIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNTcwMDIzNDE2LCJleHAiOjE1NzAwMjcwMTYsImp0aSI6ImMxMWY2YzIwMjgwZjc1YmMxZjE4NDMzM2QyZGM5NWY4MTYxYTZkNWUifQ.W_8IsTty5D7UdbcHkjrHyhNkEOyFc1r8fluvnd3kpV5wmK9Z4Tb0zv-W9DOr6mOGZUbaLvHR0Hncbqgec_iN9YNV281O3NRd-XERsn-Gf3oZ2z0Nbm5-_4NRg-WkLER4Ouo-upCd9TvXZwWqK0NNZm1Ka8N_JCzU0vb29T7lASZAZQ5POLtg3Z7PoAIk-h1HoO8Wb8acb-fkVaoLd-WR4sEhC93mxEaKe3DycXT0QtaO27GAYypz6HfWM3PsyPHio9nGr-GSt7ZNZuJYjnzqyRhXnx-H2dRggWbS6EAREWmBH2sdWe7fzMBFt_GNCl9q3yGVJQht5IOTmPDG9gixsw";
 		String response;
 
-		String BSSID[] = { "/sbin/iwconfig", " | grep 'Access Point' ", " | tr -s \" \" ", " | cut -d \" \" -f7 " };
-
-		String RSSI[] = { "/sbin/iwconfig", " | grep 'Signal level' ", " | tr -s \" \" ", " | cut -d \" \" -f5 ",
-				"cut -d\"=\" -f2" };
-
-		Process p, p2;
-		String s = "", s2="";
-		try {
-			p = Runtime.getRuntime().exec(BSSID);
-			p2 = Runtime.getRuntime().exec(RSSI);
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			s = br.readLine();
-			br = new BufferedReader(new InputStreamReader(p2.getInputStream()));
-			s2 = br.readLine();
-			System.out.println(s);
-			System.out.println(s2);
-			p.destroy();
-			p2.destroy();
-		} catch (Exception e) {
-			System.out.println("Error");
-		}
 		if (args.length == 0) {/* Menu of options is shown if no arguments are given. */
 			System.out.println("Please use one of the available options available options:");
 
@@ -51,7 +30,7 @@ public class Tester {
 			System.out.println("----------");
 			System.out.println("-poiDetails: Get Point of Interest details.");
 			System.out.println("-navigationXY: Get Navigation instructions from a given	location to a POI.");
-			System.out.println("-navPoiToPoi: Get Navigation instructions between 2	POIs");
+			System.out.println("-navPoiToPoi: Get Navigation instructions between 2 POIs");
 
 			System.out.println("\nMapping(Public)");
 			System.out.println("---------------");
@@ -75,9 +54,9 @@ public class Tester {
 			System.out.println(
 					"-radioByCoordinatesFloor: Radiomap using all the entries near the coordinate parameters.");
 			System.out.println("-radioBuidFloor: Radiomap using all the entries near the coordinate parameters.");
-			System.out.println("-estimatePosition: Estimate the location of the user.");
+			System.out.println("-estimatePosition: Estimate the location of the user. Linux ONLY.");
 		} else {
-			AnyplacePost client = new AnyplacePost("ap.cs.ucy.ac.cy", "443");
+			AnyplacePost client = new AnyplacePost("ap-dev.cs.ucy.ac.cy", "443");
 
 			if (args[0].equals("-poiDetails")) {
 				if (args.length != 2) {
@@ -219,13 +198,40 @@ public class Tester {
 				System.out.println(response + "\n"); /* .substring(0, 100) */
 			} else if (args[0].equals("-estimatePosition")) {
 				if (args.length >= 5) {
-					System.out.println("Usage: -estimatePosition <buid> <floor> <algorithm> <list of aps>");
+					System.out.println("Usage: -estimatePosition <buid> <floor> <algorithm>");
 					System.exit(0);
 				}
 				String buid = args[1];
 				String floor = args[2];
 				String algorithm = args[3];
-				String aps[] = new String[args.length - 3];
+
+				String cmd[] = { "/bin/sh", "-c",
+						"iwlist wlx90f652e5e52b scan | awk  '/Address/ {print $5}; /level/ {print $3}' |  cut -d\"=\" -f2 " };
+
+				String aps[] = new String[200];
+				Process p;
+				String s, temp;
+				int counter = 0;
+				try {
+					p = Runtime.getRuntime().exec(cmd);
+
+					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					while ((s = br.readLine()) != null && counter <= 20) {
+						temp = "{\"bssid\":\"";
+						temp += s;
+						temp += "\",\"rss\":";
+						s = br.readLine();
+						temp += s;
+						temp += "}";
+						aps[counter++] = temp;
+					}
+					p.destroy();
+					br.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				aps=Arrays.copyOf(aps, counter);
+
 				response = client.estimatePosition(buid, floor, aps, algorithm);
 				System.out.println(response + "\n"); /* .substring(0, 100) */
 			} else {
